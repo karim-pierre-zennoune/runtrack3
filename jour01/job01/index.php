ff52814b9583b4aa86042045d5fb7e7f97dbb95c
Karim Pierre ZENNOUNE <karim-pierre.zennoune@laplateforme.io>
2025-07-07 13:19:47 +0200

    JS jour01 job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+    <meta charset="UTF-8" />
+    <title>Javascript</title>
+
+    <script>
+        alert("Hello Javascript");
+
+    </script>
+  </head>
+
+
+
+  <body>
+  </body>
+
+
+</html>
